e01a98e939f4dc558bcf589a9d121c63f6473ad0
JoshuaObiz <irohjoshuaobiz@gmail.com>
2023-04-18 19:47:28 +0100

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,35 @@
-const header = document.querySelector('header')
-const closePost = document.querySelectorAll('.control .close')
-const post = document.querySelectorAll('.post')
-const messages = document.querySelector('.messages')
-const notifications = document.querySelector('.notifications')
-const notificationBtn = document.getElementById('notification')
-const closeNotifications = document.querySelector('.notifications .close')
-const messageBtn = document.getElementById('message')
-const actionBtns = document.querySelectorAll('.actions .btn')
-const followBtns = document.querySelectorAll('.suggestions .btn')
-const themeBtn = document.getElementById('theme')
-const theme = document.querySelector('.theme')
+const header = document.querySelector('header');
+const links = document.querySelectorAll('nav a')
+const closePost = document.querySelectorAll('.control .close');
+const posts = document.querySelectorAll('.post');
+const messages = document.querySelector('.messages');
+const messageBtn = document.getElementById('message');
+const notifications = document.querySelector('.notifications');
+const notificationBtn = document.getElementById('notification');
+const actionBtns = document.querySelectorAll('.actions .btn');
+const followBtns = document.querySelectorAll('.suggestions .btn');
+const themeBtn = document.getElementById('theme');
+const theme = document.querySelector('.theme');
 
+notificationBtn.addEventListener('click', () => {
+   notifications.classList.toggle('show');
+   document.querySelector('#notification .count').style.display = 'none'
+});
+
+messageBtn.addEventListener('click', ()=>{
+   // messages.style.boxShadow = 'var(--box-shadow)'
+   setTimeout(messages.style.boxShadow = 'var(--box-shadow)', 3000000)
+})
+
+links.forEach(link => {
+   link.addEventListener('click', ()=>{
+      removeActiveLink()
+      link.classList.add('active')
+   })
+});
+
+function removeActiveLink() {
+   links.forEach(link => {
+      link.classList.remove('active')
+   });
+}
