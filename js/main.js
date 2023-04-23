ebca54e16a8eaa03db19b6ad642bb50411b39db6
JoshuaObiz <irohjoshuaobiz@gmail.com>
2023-04-23 20:46:56 +0100

    started project

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const header = document.querySelector('header');
-const links = document.querySelectorAll('nav a')
+const links = document.querySelectorAll('nav a');
 const closePost = document.querySelectorAll('.control .close');
 const posts = document.querySelectorAll('.post');
 const messages = document.querySelector('.messages');
@@ -13,23 +13,37 @@ const theme = document.querySelector('.theme');
 
 notificationBtn.addEventListener('click', () => {
    notifications.classList.toggle('show');
-   document.querySelector('#notification .count').style.display = 'none'
+   document.querySelector('#notification .count').style.display = 'none';
 });
 
-messageBtn.addEventListener('click', ()=>{
+messageBtn.addEventListener('click', () => {
    // messages.style.boxShadow = 'var(--box-shadow)'
-   setTimeout(messages.style.boxShadow = 'var(--box-shadow)', 3000000)
-})
+   setTimeout((messages.style.boxShadow = 'var(--box-shadow)'), 3000000);
+});
 
-links.forEach(link => {
-   link.addEventListener('click', ()=>{
-      removeActiveLink()
-      link.classList.add('active')
-   })
+links.forEach((link) => {
+   link.addEventListener('click', () => {
+      removeActiveLink();
+      link.classList.add('active');
+   });
 });
 
 function removeActiveLink() {
-   links.forEach(link => {
-      link.classList.remove('active')
+   links.forEach((link) => {
+      link.classList.remove('active');
    });
 }
+
+followBtns.forEach((followBtn) => {
+   followBtn.addEventListener('click', () => {
+      const initialText = 'Follow';
+
+      if (
+         followBtn.textContent.toLowerCase().includes(initialText.toLowerCase())
+      ) {
+         followBtn.textContent = 'Following';
+      } else {
+         followBtn.textContent = initialText;
+      }
+   });
+});
