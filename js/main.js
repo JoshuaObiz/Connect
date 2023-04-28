764b039a412e174e8b48c2324a2e0d41004a5cdf
JoshuaObiz <irohjoshuaobiz@gmail.com>
2023-04-28 14:01:32 +0100

    added customization functionality

--- js/main.js
+++ js/main.js
@@ -1,29 +1,36 @@
 const header = document.querySelector('header');
 const links = document.querySelectorAll('nav a');
+
 const closePost = document.querySelectorAll('.post .close');
 const posts = document.querySelectorAll('.post');
-const messages = document.querySelector('.messages');
-const messageBtn = document.getElementById('message');
-const notifications = document.querySelector('.notifications');
-const notificationBtn = document.getElementById('notification');
 const actionBtns = document.querySelectorAll('.actions .btn');
 const likeBtns = document.querySelectorAll('.actions .like');
 const saveBtns = document.querySelectorAll('.actions .save');
 
+const messages = document.querySelector('.messages');
+const messageBtn = document.getElementById('message');
+
+const notifications = document.querySelector('.notifications');
+const notificationBtn = document.getElementById('notification');
+
 const followBtns = document.querySelectorAll('.suggestions .btn');
+
 const themeBtn = document.getElementById('theme');
 const theme = document.querySelector('.theme');
 const themeColors = document.querySelectorAll('.theme-color span');
-const background = document.querySelectorAll('.bg .btn');
-const fontSize = document.querySelector('.font-size span');
+const themeBg = document.querySelectorAll('.bg .btn');
+const fontSize = document.querySelectorAll('.font-size span');
 
 const body = document.querySelector('body')
+var root = document.querySelector(':root')
 
 body.addEventListener('click', (e)=>{
    if (e.target.classList.contains('theme')) {   
       theme.style.display = 'none'
+      document.querySelector('nav a:first-child').classList.add('active')
+      themeBtn.classList.remove('active')
    }
-   if (e.target.classList != 'notifications' && e.target.id != 'notification' && e.target.className != 'eva eva-bell-outline') {
+   if (e.target.classList != 'notifications' && e.target.id != 'notification' && e.target.className != 'eva eva-bell-outline' && e.target.innerHTML != `notifications`) {
       notifications.classList.remove('show')
       if (notificationBtn.classList.contains('active')) {
          document.querySelector('nav a:first-child').classList.add('active')
@@ -38,12 +45,6 @@ links.forEach((link) => {
       link.classList.add('active');
    });
 });
-
-function removeActiveLink() {
-   links.forEach((link) => {
-      link.classList.remove('active');
-   });
-}
 
 messageBtn.addEventListener('click', () => {
    messages.style.boxShadow = 'var(--box-shadow)'
@@ -98,3 +99,100 @@ followBtns.forEach((followBtn) => {
 themeBtn.addEventListener('click', ()=>{
    theme.style.display = 'flex'
 })
+
+themeColors.forEach(color => {
+   color.addEventListener('click', (e)=>{
+      removeActiveColor()
+      color.classList.add('active')
+      if (e.target.classList.contains('color1')) {
+         root.style.setProperty('--hue', '340')
+      }
+      if (e.target.classList.contains('color2')) {
+         root.style.setProperty('--hue', '55')
+      }
+      if (e.target.classList.contains('color3')) {
+         root.style.setProperty('--hue', '210')
+      }
+      if (e.target.classList.contains('color4')) {
+         root.style.setProperty('--hue', '150')
+      }
+      if (e.target.classList.contains('color5')) {
+         root.style.setProperty('--hue', '270')
+      }
+   })
+});
+
+fontSize.forEach(size => {
+   size.addEventListener('click', (e)=>{
+      removeActiveSize()
+      size.classList.add('active')
+      if (e.target.classList.contains('font1')) {
+         root.style.setProperty('--md-font', '0.9843750.rem')
+      }
+      if (e.target.classList.contains('font2')) {
+         root.style.setProperty('--md-font', '0.921875rem')
+      }
+      if (e.target.classList.contains('font3')) {
+         root.style.setProperty('--md-font', '0.85rem')
+      }
+      if (e.target.classList.contains('font4')) {
+         root.style.setProperty('--md-font', '0.8125rem')
+      }
+      if (e.target.classList.contains('font5')) {
+         root.style.setProperty('--md-font', '0.75rem')
+      }
+   })
+});
+
+themeBg.forEach(theme => {
+   theme.addEventListener('click', (e)=>{
+      removeActiveTheme()
+      theme.classList.add('active')
+      if (e.target.classList.contains('bg1')) {
+         root.style.setProperty('--light', 'hsl(210, 30%, 98%)')
+         root.style.setProperty('--white', 'hsl(210, 30%, 100%)')
+         root.style.setProperty('--dark', 'hsl(210, 30%, 25%)')
+         root.style.setProperty('--black', 'hsl(210, 30%, 10%)')
+      }
+      if (e.target.classList.contains('bg2')) {
+         root.style.setProperty('--light', 'hsl(210, 30%, 10%)')
+         root.style.setProperty('--white', 'hsl(210, 30%, 15%)')
+         root.style.setProperty('--dark', 'hsl(210, 30%, 100%)')
+         root.style.setProperty('--black', 'hsl(210, 30%, 98%)')
+      }
+      if (e.target.classList.contains('bg3')) {
+         root.style.setProperty('--light', 'hsl(210, 25%, 5%)')
+         root.style.setProperty('--white', 'hsl(210, 30%, 8%)')
+         root.style.setProperty('--dark', 'hsl(210, 30%, 100%)')
+         root.style.setProperty('--black', 'hsl(210, 30%, 98%)')
+      }
+   })
+});
+
+
+
+function removeActiveLink() {
+   links.forEach((link) => {
+      link.classList.remove('active');
+   });
+}
+
+function removeActiveColor() {
+   themeColors.forEach((color) => {
+      color.classList.remove('active');
+   });
+}
+
+function removeActiveSize() {
+   fontSize.forEach((size) => {
+      size.classList.remove('active');
+   });
+}
+
+function removeActiveTheme() {
+   themeBg.forEach((theme) => {
+      theme.classList.remove('active');
+   });
+}
+
+// console.log(root.style);
